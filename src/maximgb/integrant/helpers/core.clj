70fa988deb162be1a6123b6bb3bdbc2a35d5ab14
Maxim Bazhenov <max.bazhenov@gmail.com>
2021-07-17 02:19:34 +0300

    Docstrings moved to correct place

--- src/maximgb/integrant/helpers/core.clj
+++ src/maximgb/integrant/helpers/core.clj
@@ -2,37 +2,42 @@
 
 (def config-fn-keyword ::config-fn)
 
-(defmacro config-fn [& fn-tail]
-  "Creates a system configuration function, if sub-system/service recieves such a function as configuration value,
+(defmacro config-fn 
+  "Creates a system configuration function. If sub-system/service recieves such a function as configuration value,
    then it will call that function with service configuration map as the only parameter and use the value returned
    as configuration option value."
+  [& fn-tail]
   `(as-config-fn (fn ~@fn-tail)))
 
 
-(defn as-config-fn [fn]
+(defn as-config-fn 
   "Marks a `fn` function as system configuration function, see `config-fn` macro for more information."
+  [fn]
   (with-meta fn {config-fn-keyword true}))
 
 
-(defn is-config-fn? [fn]
-  "Checks if given `fn` should be threated as system configuration function."
+(defn is-config-fn? 
+  "Checks if given `fn` should be treated as system configuration function."
+  [fn]
   (-> fn
       (meta)
       (config-fn-keyword)
       (= true)))
 
 
-(defn realize-value [v config]
+(defn realize-value 
   "Realizes system configuraiton value.
    Checks if the passed `v` value is a system configuration function and if it is then applies the function to `config`
    and returns the result otherwise returns `v` unmodified."
+  [v config]
   (if (and (fn? v) (is-config-fn? v))
     (v config)
     v))
 
 
-(defn realize-config [config]
-  "Sames as `realize-value` but realizes entire sub-system/service configuration map."
+(defn realize-config 
+  "Same as `realize-value` but realizes entire sub-system/service configuration map."
+  [config]
   (let [ks (keys config)]
     (zipmap ks (map #(realize-value (get config %)
                                     config)
